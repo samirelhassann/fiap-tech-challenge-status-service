f33654109a935f2df3255c96f29278babffe1271
Samir El Hassan <samirelhassann@gmail.com>
2024-05-20 11:00:59 -0300

    ci: adjust env variables

--- src/drivers/webserver/app.ts
+++ src/drivers/webserver/app.ts
@@ -1,5 +1,7 @@
 import fastify from "fastify";
 
+import { env } from "@/config/env";
+
 import { cookieConfig } from "./config/cookieConfig";
 import { errorHandling } from "./config/errorHandling";
 import { jwtConfig } from "./config/jwtConfig";
@@ -7,6 +9,8 @@ import { redocConfig } from "./config/redocConfig";
 import { routes } from "./config/routes";
 import { subscribers } from "./config/subscribers";
 import { swaggerConfig } from "./config/swaggerConfig";
+
+console.log(`• [LOG] - env`, JSON.stringify(env, null, 2));
 
 export const app = fastify();
 
